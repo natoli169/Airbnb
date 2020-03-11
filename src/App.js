6acbc9856e2270262ddeda82c30f514a7f2e8272
nattigy <nathnaelakale@gmail.com>
2020-03-11 15:19:10 +0300

    (feat) add search places list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
-import { Navbar } from "./components/navbar/index";
-import { ExploreCards } from "./components/explore-cards/index";
-import { Footer } from "./components/footer/index";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <ExploreCards />
-      <Footer />
+
     </>
   );
 }
---
+++ src/components/search-places/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Container} from "./style";
+
+export const SearchList = () => {
+  return <Container>search list</Container>
+};
