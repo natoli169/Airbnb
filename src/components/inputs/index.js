9cfe77e3c2aa8908f5460a1a7e3aef1f824785d9
nattigy <nathnaelakale@gmail.com>
2020-03-11 15:04:36 +0300

    (feat) add custom input styles

---
+++ src/components/inputs/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {InputButton, InputText, Search} from "./style";
+
+export const TextField = () => {
+  return <InputText/>
+};
+
+export const Button = () => {
+  return <InputButton>Button</InputButton>
+};
+
+export const NavSearch = () => {
+  return <Search/>
+};
---
+++ src/components/inputs/style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const InputText = styled.input`
+    width: 100%;
+    height: 35px;
+    border-radius: 3px;
+    border-color: #8b8b8b38;
+    background-color: white;
+    border-width: 1px;
+    border-style: solid;
+    padding: 5px 10px;
+    outline: none;
+`;
+
+export const InputButton = styled.button`
+    width: 100%;
+    height: 35px;
+    border-radius: 3px;
+    border-width: 1px;
+    border-style: solid;
+    padding: 5px 10px;
+    outline: none;
+    background-color: crimson;
+    color: white;
+`;
+
+export const Search = styled.input`
+    max-width: 250px;
+    width: 100%;
+    height: 35px;
+    border-radius: 3px;
+    border-color: #8b8b8b38;
+    background-color: white;
+    border-width: 1px;
+    border-style: solid;
+    padding: 5px 10px;
+    outline: none;
+    box-shadow: #00000030 0px 0px 7px 0px;
+`;
